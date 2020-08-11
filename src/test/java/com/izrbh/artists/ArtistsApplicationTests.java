b240d237ef2e2030357b5ab51b46076c954c0537
xuping <wuxupingvip@126.com>
2020-08-11 16:19:41 +0800

    The MVNW file and the MVNW.cMD file and the MVN folder are ignored

---
+++ src/test/java/com/izrbh/artists/ArtistsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izrbh.artists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
